9829cd0f12b6d5d28af662f5de1bebccbff7e575
icaroxavier <icarogabriel999@icloud.com>
2024-10-06 15:21:25 -0300

    react native app with expo

---
+++ src/app/index/index.tsx
@@ -0,0 +1,22 @@
+import { Image, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import { colors } from '@/styles/colors';
+import { Category } from '@/components/category';
+
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require('@/assets/logo.png')} style={styles.logo} />
+                <TouchableOpacity >
+                    <MaterialIcons name='add' size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+
+            <Category />
+        </View>
+    )
+}
+
---
+++ src/app/index/styles.ts
@@ -0,0 +1,25 @@
+import { colors } from "@/styles/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        paddingTop: 62,
+        flex: 1,
+    },
+    title: {
+        color: colors.green[900],
+        fontSize: 30
+    },
+    header: {
+        paddingHorizontal: 24,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 32
+    },  
+    logo: {
+        height: 32,
+        width: 38
+    }
+})
